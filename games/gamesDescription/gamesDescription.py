11d2e79d55965c23eea848b2e091f6b229fcc57c
Senujan18 <ssen281@aucklanduni.ac.nz>
2023-08-25 23:29:57 +1200

    Added route to the blueprint to display games and created methods in the service layer.

--- games/gamesDescription/gamesDescription.py
+++ games/gamesDescription/gamesDescription.py
@@ -5,11 +5,12 @@ from markupsafe import escape
 
 games_description_blueprint = Blueprint('games_description_bp', __name__)
 
+
 @games_description_blueprint.route('/games-description/<int:game_id>', methods=['GET'])
 def games_description(game_id):
     get_game = services.get_game(repo.repo_instance, game_id)
-    #get_game = services.get_game(repo.repo_instance)
-    return render_template('gameDesc.html', game=get_game)
+    #get_similar_games = None
+    if len(get_game.genres) > 0:
+        get_similar_games = services.similar_game(repo.repo_instance, get_game.genres)
 
-
-
+    return render_template('gameDesc.html', game=get_game, similar_games=get_similar_games[0:4])
--- games/gamesDescription/services.py
+++ games/gamesDescription/services.py
@@ -2,3 +2,6 @@ from games.adapters.repository import AbstractRepository
 
 def get_game(repo: AbstractRepository, id):
     return repo.get_games_by_id(id)
+
+def similar_game(repo: AbstractRepository, genre):
+    return repo.get_similar_games(genre)
