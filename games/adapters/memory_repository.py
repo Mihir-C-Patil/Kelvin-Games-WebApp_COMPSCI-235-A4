b5a4aa5bda6e6d9d12320ea9194f318b5ab11ddc
Mihir-C-Patil <mightymihir100@gmail.com>
2023-08-28 22:07:46 +1200

    Added genre support in MemoryRepository
    Extended the MemoryRepository to support genres. This includes methods to add genres, retrieve a list of all genres, and get games associated with a specific genre. This refactoring is required to provide genre-based filtration for our game list feature.

--- games/adapters/memory_repository.py
+++ games/adapters/memory_repository.py
@@ -2,7 +2,7 @@ import os.path
 from bisect import insort_left
 from typing import List
 
-from games.domainmodel.model import Game
+from games.domainmodel.model import Game, Genre
 from games.adapters.datareader.csvdatareader import GameFileCSVReader
 from games.adapters.repository import AbstractRepository
 
@@ -10,12 +10,16 @@ from games.adapters.repository import AbstractRepository
 class MemoryRepository(AbstractRepository):
     def __init__(self, message=None):
         self.__games = list()
+        self.__genres = list()
 
     def add_game(self, game: Game):
         if isinstance(game, Game):
             insort_left(self.__games, game)
 
     def get_games(self) -> List[Game]:
+        return self.__games
+
+    def get_slide_games(self) -> List[Game]:
         return self.__games
 
     def get_number_of_games(self):
@@ -27,6 +31,27 @@ class MemoryRepository(AbstractRepository):
                         in game.title.lower()]
         return game_results
 
+    def add_genre(self, genre: Genre):
+        if isinstance(genre, Genre) and genre not in self.__genres:
+            insort_left(self.__genres, genre)
+
+    def get_genres(self) -> List[Genre]:
+        return self.__genres
+
+    def get_genre_of_games(self, target_genre):
+        matching_game_genre = []
+        try:
+            for game in self.__games:
+                if game.genres == target_genre:
+                    matching_game_genre.append(game)
+                else:
+                    break
+        except ValueError:
+            # No game for specified genre. Simply return an empty list.
+            pass
+
+        return matching_game_genre
+
 
 def populate(repo: AbstractRepository):
     directory_name = os.path.dirname(os.path.abspath(__file__))
@@ -37,5 +62,9 @@ def populate(repo: AbstractRepository):
     games = reader.dataset_of_games
     for game in games:
         repo.add_game(game)
+        for genre in game.genres:
+            # Check if I need to add genre directly without creating Genre obj
+            genre_ojb = Genre(genre)
+            repo.add_genre(genre)
         for genre in game.genres:
             repo.add_genre(genre)
