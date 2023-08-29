61c241d1ac4e2a8022571f1b68a3e4b2e78dd760
AzizPatel786 <m.azizp23@gmail.com>
2023-08-29 17:17:21 +1200

    Sidebar opens when mouse hovers near left border.

--- games/static/js/hamburger.js
+++ games/static/js/hamburger.js
@@ -1,32 +1,48 @@
 const sidebar = document.querySelector(".sidebar");
 const toggle = document.querySelector(".header__hamburger");
-const topLine = document.querySelector(".hamburger__top")
-const middleLine = document.querySelector(".hamburger__middle")
-const bottomLine = document.querySelector(".hamburger__bottom")
-const overlay = document.querySelector(".overlay")
+const topLine = document.querySelector(".hamburger__top");
+const middleLine = document.querySelector(".hamburger__middle");
+const bottomLine = document.querySelector(".hamburger__bottom");
+const overlay = document.querySelector(".overlay");
+const hoverThreshold = 20;
 
 toggle.addEventListener('click', () => {
-    const visible = sidebar.getAttribute('data-visible');
-    if (visible === "false") {
-        sidebar.setAttribute('data-visible', true);
-        topLine.classList.add("rotate")
-        middleLine.classList.add("hide")
-        bottomLine.classList.add("rotate-minus")
-        overlay.classList.remove("hide")
-    } else {
-        sidebar.setAttribute('data-visible', false);
-        topLine.classList.remove("rotate")
-        middleLine.classList.remove("hide")
-        bottomLine.classList.remove("rotate-minus")
-        overlay.classList.add("hide")
-    }
-})
+    toggleSidebar();
+});
 
 overlay.addEventListener('click', () => {
-    sidebar.setAttribute('data-visible', false);
-    topLine.classList.remove("rotate")
-    middleLine.classList.remove("hide")
-    bottomLine.classList.remove("rotate-minus")
-    overlay.classList.add("hide")
-})
+    toggleSidebar();
+});
 
+document.addEventListener('mousemove', (event) => {
+    const mouseX = event.clientX;
+
+    if (mouseX <= hoverThreshold) {
+        openSidebar();
+    }
+});
+
+function openSidebar() {
+    sidebar.setAttribute('data-visible', true);
+    topLine.classList.add("rotate");
+    middleLine.classList.add("hide");
+    bottomLine.classList.add("rotate-minus");
+    overlay.classList.remove("hide");
+}
+
+function closeSidebar() {
+    sidebar.setAttribute('data-visible', false);
+    topLine.classList.remove("rotate");
+    middleLine.classList.remove("hide");
+    bottomLine.classList.remove("rotate-minus");
+    overlay.classList.add("hide");
+}
+
+function toggleSidebar() {
+    const visible = sidebar.getAttribute('data-visible');
+    if (visible === "false") {
+        openSidebar();
+    } else {
+        closeSidebar();
+    }
+}
