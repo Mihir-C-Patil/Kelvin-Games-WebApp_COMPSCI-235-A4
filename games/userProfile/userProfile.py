3f4cde41eb5fb55ffc8d80f75710d2327bfce8bd
AzizPatel786 <m.azizp23@gmail.com>
2023-09-12 10:02:38 +1200

    Creating base layout for user profile page.

--- games/userProfile/userProfile.py
+++ games/userProfile/userProfile.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
 import games.adapters.repository as repo
+from games.gameLibrary import services
 
 from games.gameLibrary.gameLibrary import get_genres_and_urls
 from games.gameLibrary.services import get_genres
@@ -10,5 +11,6 @@ userProfile_blueprint = Blueprint('pp_bp', __name__)
 @userProfile_blueprint.route('/userprofile', methods=['GET'])
 def view_user_profile():
     genres = get_genres(repo.repo_instance)
+    all_games = services.get_games(repo.repo_instance)
     return render_template('userProfile.html', all_genres=genres,
-                           genre_urls=get_genres_and_urls())
+                           genre_urls=get_genres_and_urls(), games=all_games)
