4a7750100ecca22749b738d5d2b1a0d13cfc531b
AzizPatel786 <m.azizp23@gmail.com>
2023-09-23 10:37:28 +1200

    Game library pages show "Remove from Wishlist" instead of add if game already in wishlist.

--- games/gameLibrary/gameLibrary.py
+++ games/gameLibrary/gameLibrary.py
@@ -1,11 +1,13 @@
 import random
 
-from flask import Blueprint, render_template, request, url_for
+from flask import Blueprint, render_template, request, url_for, session
 from flask_paginate import Pagination, get_page_args
 
 import games.adapters.repository as repo
 from games.authentication.authentication import WishlistForm
 from games.gameLibrary import services
+from games.userProfile.services import get_user_wishlist
+from games.authentication import services as authservice
 
 # Create a Flask Blueprint for the game library view
 gameLibrary_blueprint = Blueprint('viewGames_bp', __name__)
@@ -35,6 +37,8 @@ def view_games():
     pagination = Pagination(page=page, per_page=per_page, offset=offset,
                             total=len(all_games),
                             record_name='List')
+    user = authservice.get_user(session['username'], repo.repo_instance)
+    wishlist = get_user_wishlist(user)
 
     # Render the template
     return render_template('gameLibrary.html', heading='All Games',
@@ -44,7 +48,7 @@ def view_games():
                            all_genres=genres,
                            pagination=pagination,
                            genre_urls=get_genres_and_urls(),
-                           form=form)
+                           form=form, wishlist=wishlist)
 
 
 def get_genres_and_urls(sort_criteria='title'):
@@ -98,14 +102,15 @@ def games_by_genre():
         slide_genre_games = selected_genre_games[10:15]
     genres = services.get_genres(repo.repo_instance)
     form = WishlistForm()
-
+    user = authservice.get_user(session['username'], repo.repo_instance)
+    wishlist = get_user_wishlist(user)
     # Render the template
     return render_template('gameLibraryG.html', heading=target_genre,
                            games=sorted_rendered, all_genres=genres,
                            genre_urls=get_genres_and_urls(sort_criteria),
                            pagination=pagination,
                            slide_genre_games=slide_genre_games,
-                           form=form)
+                           form=form, wishlist=wishlist)
 
 
 def side_bar_genres():
