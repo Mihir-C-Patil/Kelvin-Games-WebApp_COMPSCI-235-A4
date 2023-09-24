d924f18b78d9416d217568718b5793b3bd305914
Mihir-C-Patil <mightymihir100@gmail.com>
2023-09-24 14:30:01 +1300

    Improve login and registration form validation and UI
    Enhance the user experience for the login and registration forms by updating username and password validation. Better feedback, meaningful error messages, and inclusion of genre information from the game library are provided. Login and register form specific styles moved to a new login_register.css file for maintainability and extendability. Associated changes applied to login_register.html template to align with code and stylesheet modifications. These changes form part of an overall user experience enhancement across the app.

--- games/authentication/authentication.py
+++ games/authentication/authentication.py
@@ -1,18 +1,19 @@
-from flask import (Blueprint, render_template, redirect, url_for, session,
-                   request)
+from functools import wraps
+
+from flask import (Blueprint, render_template, redirect, url_for, session)
 from flask_wtf import FlaskForm
+from password_validator import PasswordValidator
 from wtforms import StringField, PasswordField, SubmitField, HiddenField
 from wtforms.validators import DataRequired, Length, ValidationError
 
-from password_validator import PasswordValidator
-
-from functools import wraps
-
 import games.adapters.repository as repo
+from games import get_genres_and_urls
 from games.authentication import services
 from games.authentication.services import UnknownUserException
+from games.gameLibrary.services import get_genres
 
-authentication_blueprint = Blueprint('authentication_bp', __name__, url_prefix='/authentication')
+authentication_blueprint = Blueprint('authentication_bp', __name__,
+                                     url_prefix='/authentication')
 
 
 class PasswordValid:
@@ -34,7 +35,8 @@ class RegistrationForm(FlaskForm):
     username = StringField('Username',
                            [DataRequired(message='Please provide a username.'),
                             Length(min=3,
-                                   message='Too Small Bro!')])
+                                   message='Your username must be a minimum '
+                                           'of 3 characters.')])
 
     password = PasswordField('Password', [DataRequired(
         message='Please provide a password'), PasswordValid()])
@@ -51,6 +53,7 @@ class LoginForm(FlaskForm):
 def register():
     form = RegistrationForm()
     username_not_unique = None
+    genres = get_genres(repo.repo_instance)
 
     if form.validate_on_submit():
         try:
@@ -64,7 +67,9 @@ def register():
     return render_template('authentication/login_register.html',
                            title='Register', form=form,
                            username_error_message=username_not_unique,
-                           handler_url=url_for('authentication_bp.register'))
+                           handler_url=url_for('authentication_bp.register'),
+                           all_genres=genres,
+                           genre_urls=get_genres_and_urls())
 
 
 @authentication_blueprint.route('/login', methods=['GET', 'POST'])
@@ -72,13 +77,14 @@ def login():
     form = LoginForm()
     username_not_found = None
     incorrect_password = None
+    genres = get_genres(repo.repo_instance)
 
     if form.validate_on_submit():
         try:
             user = services.get_user(form.username.data, repo.repo_instance)
             if not user:
                 raise UnknownUserException
-            user = {'username': user.username, 'password': user.password}
+            user = {'username': user['username'], 'password': user['password']}
             services.authenticate_user(user['username'], form.password.data,
                                        repo.repo_instance)
 
@@ -93,7 +99,9 @@ def login():
     return render_template('authentication/login_register.html', title='Login',
                            username_error_message=username_not_found,
                            password_error_message=incorrect_password,
-                           form=form)
+                           form=form,
+                           all_genres=genres,
+                           genre_urls=get_genres_and_urls())
 
 
 @authentication_blueprint.route('/logout')
