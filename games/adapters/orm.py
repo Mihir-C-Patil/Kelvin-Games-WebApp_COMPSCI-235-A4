3096d0a3399f5a3263f4b01d8db0e318417149da
AzizP <m.azizp23@gmail.com>
2023-10-12 11:13:25 +1300

    Fixed orm.py.

--- games/adapters/orm.py
+++ games/adapters/orm.py
@@ -25,19 +25,20 @@ games_table = Table('game', metadata,
                     Column('publisher', ForeignKey('publisher.id')),
                     Column('image_url', String(1024), nullable=False),
                     Column('website_url', String(1024), nullable=False),
-                    Column('genres', ForeignKey('genre.id')),
-                    Column('tags', String(1024), nullable=False),
-                    Column('reviews', ForeignKey('review.id')))
+                    Column('tags', String(1024), nullable=False))
 
 genres_table = Table('genre', metadata,
                      Column('id', Integer, primary_key=True),
-                     Column('genre_name', String(255), nullable=False),
-                     Column('games', ForeignKey('game.id')))
+                     Column('genre_name', String(255), nullable=False))
+
+game_genres_table = Table('game_genres', metadata,
+                          Column('id', Integer, primary_key=True, autoincrement=True),
+                          Column('game_id', ForeignKey('game.id')),
+                          Column('genre_name', ForeignKey('genre.id')))
 
 publishers_table = Table('publisher', metadata,
                          Column('id', Integer, primary_key=True),
-                         Column('publisher_name', String(255), nullable=False),
-                         Column('games', ForeignKey('game.id')))
+                         Column('publisher_name', String(255), nullable=False))
 
 reviews_table = Table('review', metadata,
                       Column('id', Integer, primary_key=True),
@@ -65,29 +66,28 @@ def map_model_to_tables():
         '_Game__price': games_table.c.price,
         '_Game__release_date': games_table.c.release_date,
         '_Game__description': games_table.c.description,
-        '_Game__publisher': relationship(Publisher, foreign_keys=[games_table.c.publisher]),
+        '_Game__publisher': relationship(Publisher, foreign_keys=[games_table.c.publisher],
+                                         back_populates='_Publisher__games'),
         '_Game__image_url': games_table.c.image_url,
         '_Game__website_url': games_table.c.website_url,
-        '_Game__genres': relationship(Genre, foreign_keys=[games_table.c.genres]),
-        '_Game__tags_string': games_table.c.tags,
-        '_Game__reviews': relationship(Review, foreign_keys=[reviews_table.c.game])
+        '_Game__tags_string': games_table.c.tags
     })
     mapper(Genre, genres_table, properties={
-        '_Genre__genre_name': genres_table.c.genre_name,
-        '_Genre__games': relationship(Game, foreign_keys=[games_table.c.genres])
+        '_Genre__genre_name': genres_table.c.genre_name
     })
     mapper(Publisher, publishers_table, properties={
         '_Publisher__publisher_name': publishers_table.c.publisher_name,
-        '_Publisher__games': relationship(Game, foreign_keys=[games_table.c.publisher])
+        '_Publisher__games': relationship(Game, foreign_keys=[games_table.c.publisher],
+                                          back_populates='_Game__publisher')
     })
     mapper(Review, reviews_table, properties={
         '_Review__review_text': reviews_table.c.review_text,
         '_Review__rating': reviews_table.c.rating,
         '_Review__timestamp': reviews_table.c.timestamp,
-        '_Review__game': relationship(Game, foreign_keys=[games_table.c.id]),
-        '_Review__user': relationship(User, foreign_keys=[users_table.c.id])
+        '_Review__game': relationship(Game),
+        '_Review__user': relationship(User, foreign_keys=[reviews_table.c.user], back_populates='_User__reviews')
     })
     mapper(Wishlist, wishlists_table, properties={
         '_Wishlist__games': relationship(Game),
-        '_Wishlist__user': relationship(User, foreign_keys=[users_table.c.id])
+        '_Wishlist__user': relationship(User, foreign_keys=[wishlists_table.c.user], back_populates='_User__wishlist')
     })
