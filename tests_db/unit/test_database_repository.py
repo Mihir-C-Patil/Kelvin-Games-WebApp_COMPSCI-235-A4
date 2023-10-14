ed02cb2c1ce2006020a18130e89c6878f5e2fa1d
Senujan18 <ssen281@aucklanduni.ac.nz>
2023-10-15 02:17:51 +1300

    Added more test cases for both databases and orm

--- tests_db/unit/test_database_repository.py
+++ tests_db/unit/test_database_repository.py
@@ -1,3 +1,5 @@
+import datetime
+
 import pytest
 from games.domainmodel.model import Game, User, Genre, Review, Wishlist, Publisher
 from games.adapters.repository import RepositoryException
@@ -88,3 +90,148 @@ def test_slide_games(session_factory):
     game = Game(7940, 'Call of Duty® 4: Modern Warfare®')
     assert game in games
 
+def test_search_games_by_invalid_tags(session_factory):
+    repo = database_repository.SqlAlchemyRepository(session_factory)
+    games_by_tag = repo.search_games_by_tags('invalid')
+    assert len(games_by_tag) == 0
+
+def test_get_game_by_invalid_id(session_factory):
+    repo = database_repository.SqlAlchemyRepository(session_factory)
+    invalid_game = repo.get_games_by_id(4)
+    assert invalid_game == None
+
+def test_add_to_wishlist(session_factory):
+    repo = database_repository.SqlAlchemyRepository(session_factory)
+    user = User('Kelvin', 'ABCDEF1234')
+    user1 = User('Bob', 'Hello1234')
+    repo.add_user(user)
+    repo.add_user(user1)
+    get_user = repo.get_user('kelvin')
+    game2 = Game(1, 'Spongebob')
+    game3 = Game(2, 'Fifa')
+    repo.add_wish_game(user, game2)
+    wishlist = repo.get_wishlist(user)
+    assert user == get_user and isinstance(get_user, User)
+
+def test_user_wishlist_empty(session_factory):
+    repo = database_repository.SqlAlchemyRepository(session_factory)
+    user = User('Kelvin', 'ABCDEF1234')
+    repo.add_user(user)
+    get_wishlist = repo.get_wishlist(user)
+    assert get_wishlist == []
+
+def test_search_games_by_invalid_title(session_factory):
+    repo = database_repository.SqlAlchemyRepository(session_factory)
+    games = repo.search_games_by_title('NonExistentGame')
+    assert len(games) == 0
+
+def test_add_to_wishlist(session_factory):
+    repo = database_repository.SqlAlchemyRepository(session_factory)
+    user = User('Kelvin', 'password123')
+    game = repo.get_games_by_id(7940)
+    repo.add_user(user)
+    repo.add_game(game)
+    repo.add_wish_game(user, game)
+    wishlist = repo.get_wishlist(user)
+    assert game in wishlist
+    assert len(wishlist) == 1
+
+def test_remove_games_from_wishlist(session_factory):
+    repo = database_repository.SqlAlchemyRepository(session_factory)
+    user = User('Kelvin', 'password123')
+    game = repo.get_games_by_id(7940)
+    repo.add_user(user)
+    repo.add_game(game)
+    repo.add_wish_game(user, game)
+    game2 = repo.get_games_by_id(311120)
+    game3 = repo.get_games_by_id(418650)
+    repo.add_wish_game(user, game2)
+    repo.add_wish_game(user, game3)
+    wishlist = repo.get_wishlist(user)
+    repo.remove_wish_game(user, game2)
+    assert game2 not in wishlist
+
+def test_user_can_add_review(session_factory):
+    repo = database_repository.SqlAlchemyRepository(session_factory)
+    user = User('Kelvin', 'password123')
+    repo.add_user(user)
+    game = repo.get_games_by_id(7940)
+    repo.add_review(user, game, 5, 'Cool Game')
+    get_review = repo.get_user_review(user)
+    assert len(get_review) == 1
+
+def test_get_user_review(session_factory):
+    repo = database_repository.SqlAlchemyRepository(session_factory)
+    user = User('Kelvin', 'password123')
+    repo.add_user(user)
+    game = repo.get_games_by_id(7940)
+    repo.add_review(user, game, 5, 'Cool Game')
+    get_review = repo.get_user_review(user)
+    assert type(get_review[0]) == Review
+
+def test_user_can_add_multiple_review(session_factory):
+    repo = database_repository.SqlAlchemyRepository(session_factory)
+    user = User('Kelvin', 'password123')
+    repo.add_user(user)
+    game = repo.get_games_by_id(7940)
+    repo.add_review(user, game, 5, 'Cool Game')
+    game_1 = repo.get_games_by_id(311120)
+    repo.add_review(user, game_1, 4, 'Great!')
+    get_review = repo.get_user_review(user)
+    assert len(get_review) == 2
+
+def add_game(session_factory):
+    repo = database_repository.SqlAlchemyRepository(session_factory)
+    game = Game(1, 'spongebob')
+    game.price = 100.01
+    game.release_date = datetime.datetime
+    repo.add_game(game)
+    assert repo.get_number_of_games() == 982
+
+def add_multiple_games(session_factory):
+    repo = database_repository.SqlAlchemyRepository(session_factory)
+    game = Game(1, 'spongebob')
+    game.price = 100.01
+    game.release_date = datetime.datetime
+    game2 = Game(2, 'fifa')
+    game2.price = 100.01
+    game2.release_date = datetime.datetime
+    game3 = Game(3, 'NBA')
+    game.price = 100.01
+    game.release_date = datetime.datetime
+    repo.add_game(game)
+    assert repo.get_number_of_games() == 985
+
+def test_add_genre(session_factory):
+    repo = database_repository.SqlAlchemyRepository(session_factory)
+    genre = Genre('new_genre')
+    repo.add_genre(genre)
+    assert genre in repo.get_genres()
+
+def test_add_multiple_genres(session_factory):
+    repo = database_repository.SqlAlchemyRepository(session_factory)
+    genre = Genre('new_genre')
+    genre1 = Genre('new_genre1')
+    genre2 = Genre('new_genre2')
+    genre3 = Genre('new_genre3')
+    repo.add_genre(genre)
+    repo.add_genre(genre1)
+    repo.add_genre(genre2)
+    repo.add_genre(genre3)
+    assert genre in repo.get_genres()
+    assert genre1 in repo.get_genres()
+    assert genre2 in repo.get_genres()
+    assert genre3 in repo.get_genres()
+
+def test_add_publisher(session_factory):
+    repo = database_repository.SqlAlchemyRepository(session_factory)
+    publisher = Publisher('Kelvin Developers')
+    repo.add_publisher(publisher)
+    assert publisher in repo.get_publishers()
+
+def test_add_multiple_publisher(session_factory):
+    repo = database_repository.SqlAlchemyRepository(session_factory)
+    publisher_names = ['Hello', 'codemasters', 'ea sports']
+    for name in publisher_names:
+        repo.add_publisher(Publisher(name))
+    assert Publisher('codemasters') in repo.get_publishers()
--- tests_db/unit/test_orm.py
+++ tests_db/unit/test_orm.py
@@ -1,6 +1,7 @@
 import pytest
 from sqlalchemy.exc import IntegrityError
-from games.domainmodel.model import Game, User, Review, Genre, Wishlist
+from games.domainmodel.model import Game, User, Review, Genre, Wishlist, Publisher
+import datetime
 
 def insert_user(empty_session, values = None):
     new_name = 'Kelvin'
@@ -9,9 +10,120 @@ def insert_user(empty_session, values = None):
         new_name = values[0]
         password = values[1]
 
-    empty_session.execute('INSERT INTO user (user_name, password) VALUES (:user_name, :password)',
-                          {'user_name': new_name, 'password': password})
+    empty_session.execute('INSERT INTO user (username, password) VALUES (:username, :password)',
+                          {'username': new_name, 'password': password})
 
-    row = empty_session.execute('SELECT id from users where user_name = :user_name',
-                                {'user_name': new_name}).fetchone()
+    row = empty_session.execute('SELECT id from user where username = :username',
+                                {'username': new_name}).fetchone()
     return row[0]
+
+def insert_users(empty_session, values):
+    for value in values:
+        empty_session.execute('INSERT INTO user (username, password) VALUES (:username, :password)',
+                              {'username': value[0], 'password': value[1]})
+    rows = list(empty_session.execute('SELECT id from user'))
+    keys = tuple(row[0] for row in rows)
+    return keys
+
+def insert_game(empty_session):
+    sql = "INSERT INTO game (id, game_title, price, release_date, description, publisher, image_url, tags) VALUES (:id, :game_title, :price, :release_date, :description, :publisher, :image_url, :tags)"
+    params = {
+        'id': 454680,
+        'game_title': 'MetaTron',
+        'price': 0,
+        'release_date': 'Dec 19, 2016',
+        'description': 'You are TRON!',
+        'publisher': 'TubbyKiD UG (haftungsbeschränkt)',
+        'image_url': 'https://cdn.akamai.steamstatic.com/steam/apps/454680/ss_898bf6187d7a60a1cd59b728a9acca41cafeeebb.1920x1080.jpg?t=1545358112',
+        'tags': 'Action'
+    }
+    empty_session.execute(sql, params)
+    row = empty_session.execute('SELECT id FROM game WHERE id = 454680').fetchone()
+    return row[0]
+
+def insert_game_review(empty_session):
+    game_key = insert_game(empty_session)
+    user_key = insert_user(empty_session)
+    timestamp_1 = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    empty_session.execute(
+        'INSERT INTO review (user, game, rating, comment, timestamp) VALUES '
+        '(:user_id, :game_id, 5, "Great Game", :timestamp_1)',
+        {'user_id': user_key, 'game_id': game_key, 'timestamp_1': timestamp_1}
+    )
+    row = empty_session.execute('SELECT id from game').fetchone()
+    return row[0]
+
+def insert_publisher(empty_session):
+    name = 'Kelvin Developers'
+    empty_session.execute(
+        'INSERT INTO publisher (publisher_name) VALUES '
+            '(:publisher_name)',
+    {'publisher_name': name}
+    )
+    row = empty_session.execute('SELECT publisher_name from publisher WHERE publisher_name = "Kelvin Developers"').fetchone()
+    return row[0]
+
+
+def make_game():
+    game = Game(454680, 'MetaTron')
+    return game
+
+def make_genre():
+    genre = Genre("Adventure")
+    return genre
+
+def make_publisher():
+    pub = Publisher('Kelvin Developers')
+    return pub
+
+def insert_genre(empty_session):
+    empty_session.execute('INSERT INTO genre(genre_name) VALUES ("Adventure")')
+    row = empty_session.execute('SELECT genre_name FROM genre where genre_name = "Adventure"').fetchone()
+    return row[0]
+
+
+def test_loading_users(empty_session):
+    users = list()
+    users.append(("andrew", "ABCDE1223"))
+    users.append(("cindy", "ABCDE1223"))
+    insert_users(empty_session, users)
+
+    expected = [
+        User("andrew", "ABCDE1223"),
+        User("cindy", "ABCDE1223")
+    ]
+    assert empty_session.query(User).all() == expected
+
+def test_loading_game(empty_session):
+    game_key = insert_game(empty_session)
+    expected_game = make_game()
+    fetched_game = empty_session.query(Game).one()
+    assert fetched_game == expected_game
+
+def test_review_games(empty_session):
+    insert_game_review(empty_session)
+    rows = empty_session.query(Game).all()
+    game = rows[0]
+    for review in game.reviews:
+        assert type(review) == Review
+        assert review.comment == 'Great Game'
+
+def test_loading_of_genres(empty_session):
+    genre = insert_genre(empty_session)
+    expected_genre = make_genre()
+    rows = empty_session.query(Genre).all()
+    genre = rows[0]
+    assert genre == expected_genre
+
+def test_loading_publishers(empty_session):
+    publisher = insert_publisher(empty_session)
+    expected_pub = make_publisher()
+    rows = empty_session.query(Publisher).all()
+    publisher = rows[0]
+    assert publisher == expected_pub
+    assert type(publisher) == Publisher
+
+def test_persistence_review(empty_session):
+    game_key = insert_game(empty_session)
+    user_key = insert_user(empty_session)
+    rows = empty_session.query(Game).all()
